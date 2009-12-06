70c76aa9d611b490eb3258a39cdb39564d681d26
Yehuda Katz <wycats@Yehuda-Katz.local>
2009-12-06 00:30:53 -0800

    Added tests for streaming tasks and waits for exit tasks; TODO: MrTask.launch

---
+++ examples/simple.rb
@@ -0,0 +1,28 @@
+framework 'Cocoa'
+
+$:.push "#{Dir.pwd}/lib"
+require "mr_notification"
+require "mr_task"
+
+task = MrTask.new("/bin/cat", with_directory:"/bin/") do |output, notification|
+  puts "finished task"
+  puts output
+  task = notification.object
+  exit
+end
+
+task.launch(File.expand_path("~/.profile"))
+
+task = MrTask.new("/usr/bin/tail")
+
+task.on_output do |output|
+  puts output
+end
+
+`touch #{Dir.pwd}/log.log`
+
+File.open("#{Dir.pwd}/log.log", "w") do |file|
+  task.launch("-f", "#{Dir.pwd}/log.log")
+end
+
+NSApplication.sharedApplication.run
--- test/task_test.rb
+++ test/task_test.rb
@@ -18,12 +18,17 @@ class TestMrTask < Test::Unit::TestCase
     @result = result
   end
 
+  def test_task_doesnt_segfault_with_invalid_executable
+    assert_raises(MrTask::InvalidExecutable) { MrTask.new("/bin/mrinvalid") }
+    assert_raises(MrTask::InvalidExecutable) do
+      MrTask.new("/bin/mrinvalid", with_directory:"/")
+    end
+  end
+
   def test_simple_task_doesnt_explode
     task = MrTask.new("/bin/ls")
     assert_nothing_raised { task.launch }
   end
-
-  require "monitor"
 
   def test_task_triggers_output_event
     task = MrTask.new("/bin/ls").on_output do |output|
@@ -38,9 +43,44 @@ class TestMrTask < Test::Unit::TestCase
     task = MrTask.new("/bin/ls").on_output do |output|
       set_async_result(output)
     end
-  
+
     task.launch("/")
     assert_match %r{Applications}, async_result
   end
 
+  def test_task_waits_for_exit_and_calls_block_with_output
+    task = MrTask.new("/bin/ls") do |output|
+      set_async_result(output)
+    end
+
+    task.launch
+    assert_match %r{lib}, async_result
+  end
+
+  def test_task_with_directory_waits_for_exit_and_calls_block_with_output
+    task = MrTask.new("/bin/ls", with_directory:"/") do |output|
+      set_async_result(output)
+    end
+
+    task.launch
+    assert_match %r{Applications}, async_result
+  end
+
+  def test_task_works_with_a_directory
+    run, result = 0, ""
+    file = File.join(File.dirname(__FILE__), "log.log")
+
+    task = MrTask.new("/usr/bin/tail").on_output do |output|
+      File.open(file, "a") {|f| f.puts "into log" }
+      run += 1
+      result << output
+      set_async_result(result) if run == 3
+    end
+
+    task.launch("-f", file)
+    File.open(file, "w") {|f| f.puts "into log" }
+    assert_match /(into log\n){3}/, async_result
+  ensure
+    File.delete(file)
+  end
 end
